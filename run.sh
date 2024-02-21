26c47904f07f79af78cdfeb25196c8259093375d
Peter <peter@kokosnoot>
2024-02-21 20:21:07 +0100

    betere manier van env-vars

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 cd /home/peter/dev/slackbot
-. ./setslackenv.sh
 docker stop slackbot
 docker rm -f slackbot
 docker run \
 	--detach \
 	--restart always \
 	--name slackbot \
-	--env SLACK_ID_RASPBOT=${SLACK_ID_RASPBOT} \
+	--env-file env.list \
 	slackbot
