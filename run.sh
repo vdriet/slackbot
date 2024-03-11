16d9038ce3963614344e14458da88193ae05525c
Peter <peter@kokosnoot>
2024-03-11 20:39:18 +0100

    slackbot via docker compose

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-docker stop slackbot
-docker rm -f slackbot
-docker run \
-	--detach \
-	--restart always \
-	--name slackbot \
-	--env-file env.list \
-	slackbot
+cd ../webserver
+docker compose up -d
